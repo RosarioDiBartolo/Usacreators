3ba56e195b011c7bc31c83826a8416b9b44c8bc8
Rosario <marcodibartolo2@gmail.com>
2025-11-06 14:27:20 +0100

    successfully migrating to nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // oppure @vitejs/plugin-react-swc
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import vercel from "vite-plugin-vercel";
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tanstackRouter(),
-    vercel({
-      entries: {
-        ssr: { id: "endpoints/ssr.ts", name: "ssr", route: "/api/ssr" },
-        hello: { id: "endpoints/api/hello.ts", name: "hello", route: "/api/hello" },
-        creator: { id: "endpoints/api/creator.ts", name: "creator", route: "/api/creator" },
-      },
-      rewrites: [
-        { source: "/api/(.*)", destination: "/api/$1" },
-        { source: "/assets/(.*)", destination: "/assets/$1" },
-        { source: "/(.*)", destination: "/api/ssr" },
-      ],
-      defaultSupportsResponseStreaming: true,
-    }),
-  ],
-});
+  plugins: [tanstackStart(), nitro(), react()],
+})
