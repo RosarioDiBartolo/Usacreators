2fb59b07033252f4e78b315d36f2a743b111b65b
Rosario <marcodibartolo2@gmail.com>
2025-11-17 21:40:59 +0100

    catalog update

--- src/routes/catalog.tsx
+++ src/routes/catalog.tsx
@@ -2,8 +2,8 @@ import CreatorsCarousel from "@/components/creators-carousel";
 import { creatorsQueryOptions } from "@/lib/creators/get-creators";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
+import PricingSection from "@/components/ui/pricing-section";
 export const Route = createFileRoute("/catalog")({
   loader: ({ context }) =>
     context.queryClient.ensureQueryData(creatorsQueryOptions),
@@ -16,13 +16,13 @@ function CatalogPage() {
 
   return (
     <>
-       <main className="    pt-20  text-center">
+       <main className=" pt-20  text-center">
         <h2>We already acquired the best content creators.</h2>
         <h1>You just have to choose...</h1>
         <CreatorsCarousel creators={creators} />
         <Button size={"2xl"}>Find the creator for your need.</Button>
-        <div>
-          
+        <div className=" relative">
+          <PricingSection />
         </div>
       </main>
     </>
